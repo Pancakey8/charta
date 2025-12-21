aca933a8d7d6fcbe393cdc1337df5b02a63bf86b
Pancakey8 <pancake-08@proton.me>
2025-12-21 16:13:48 +0300

    New behaviour system attempt

--- app/Analyzer.hs
+++ app/Analyzer.hs
@@ -1,153 +1,86 @@
+{-# LANGUAGE LambdaCase    #-}
 {-# LANGUAGE TupleSections #-}
-{-# LANGUAGE LambdaCase #-}
 module Analyzer where
 
-import Traverser (Instruction (..))
+import           Control.Monad ((>=>))
+import           Core          (FuncTable, arithmetic)
+import           Data.Either   (isLeft, lefts, rights)
+import           Data.Maybe    (mapMaybe)
+import           Traverser     (Instruction (..))
 import qualified Data.Map as M
-import Core (FuncTable, Function (..))
-import qualified Data.Vector as V
-import Debug.Trace (trace)
-import Data.List (isPrefixOf)
-import Data.Foldable (find, forM_)
-import Parser (Arguments (..))
-import Control.Monad (foldM)
-import Data.Maybe (mapMaybe)
 
-data Type = TInt
-          | TFloat
-          | TGeneric String
-          deriving (Show, Eq)
+data MiniValue = MInt
+               | MFloat
+               | MChar
+               | MBool (Maybe Bool)
+               deriving (Show)
 
-allTypes :: [Type]
-allTypes = [TInt, TFloat]
+type Mapping = ([MiniValue], [MiniValue])
 
-type Mapping = ([Type], [Type])
-newtype Behaviour = Bhv [Mapping]
-                  deriving (Show, Eq)
+idMap :: [MiniValue] -> Mapping
+idMap args = (args, args)
 
-arithmeticBehav :: [Mapping]
-arithmeticBehav =
+newtype Behaviour = Bhv { runBhv :: [Mapping] -> Either String [Mapping] }
+
+collectResults :: [Either e [a]] -> Either e [a]
+collectResults xs =
+  let
+    rs = concat (rights xs)
+    ls = lefts xs
+  in
+    case rs of
+      [] -> case ls of
+              (e:_) -> Left e
+              []    -> Right []
+      _ -> Right rs
+
+liftBhv :: ([MiniValue] -> Either String [MiniValue]) -> Behaviour
+liftBhv f = Bhv $ \maps -> collectResults $ [ fmap (\outs' -> [(ins, outs')]) (f outs) | (ins, outs) <- maps ]
+
+andThen :: Behaviour -> Behaviour -> Behaviour
+(Bhv f) `andThen` (Bhv g) = Bhv (f >=> g)
+
+pushElem :: MiniValue -> Behaviour
+pushElem x = liftBhv $ \stk -> pure $ x:stk
+
+branch :: Behaviour -> Behaviour -> Behaviour
+branch success fail = Bhv $ \maps ->
+  collectResults (map branchOne maps)
+  where
+    branchOne (i, o) =
+      case o of
+        MBool (Just True):xs -> runBhv success [(i, xs)]
+        MBool (Just False):xs -> runBhv fail [(i, xs)]
+        MBool Nothing:xs ->
+          liftA2 (++) (runBhv success [(i, xs)]) (runBhv fail [(i, xs)])
+        _ ->
+          Left "branch: Expected bool"
+
+isChar :: Behaviour
+isChar = liftBhv $
+  \case
+    (MChar:xs) -> pure $ MBool (Just True):MChar:xs
+    s@(_:xs) -> pure $ MBool (Just False):s
+    _ -> Left "isChar: Expected any"
+
+arithmBehav :: Behaviour
+arithmBehav = liftBhv $
+  \case
+    (MInt:MInt:xs) -> pure $ MInt:xs
+    (MFloat:MInt:xs) -> pure $ MFloat:xs
+    (MInt:MFloat:xs) -> pure $ MFloat:xs
+    (MFloat:MFloat:xs) -> pure $ MFloat:xs
+    _ -> Left "arithmetic: Expected numbers"
+
+internalFns :: M.Map String Behaviour
+internalFns = M.fromList $ concatMap (\(names, behav) -> [ (name, behav) | name <- names ]) $
   [
-    ([TInt, TInt], [TInt]),
-    ([TFloat, TInt], [TFloat]),
-    ([TInt, TFloat], [TFloat]),
-    ([TFloat, TFloat], [TFloat])
+    (["¿char"], isChar),
+    (["+"], arithmBehav),
+    (["-"], arithmBehav),
+    (["*"], arithmBehav),
+    (["/"], arithmBehav)
   ]
-
-internals :: M.Map String Behaviour
-internals = M.fromList $ concatMap (\(names, maps) -> [ (name, Bhv maps) | name <- names ]) $
-  [
-    (["+"], arithmeticBehav),
-    (["-"], arithmeticBehav),
-    (["*"], arithmeticBehav),
-    (["/"], arithmeticBehav),
-    (["↕", "swp"], [([TGeneric "a", TGeneric "b"], [TGeneric "b", TGeneric "a"])]),
-    (["↻", "rot"], [([TGeneric "a", TGeneric "b", TGeneric "c"], [TGeneric "c", TGeneric "a", TGeneric "b"])]),
-    (["⇈", "dup"], [([TGeneric "a"], [TGeneric "a", TGeneric "a"])])
-  ]
-
-collapseMap :: String -> Type -> Mapping -> Mapping
-collapseMap gen t (ins, outs) = (map fix ins, map fix outs)
-  where
-    fix (TGeneric g) = if g == gen
-                       then t
-                       else TGeneric g
-    fix a = a
-
-collapseBhv :: String -> Type -> Behaviour -> Behaviour
-collapseBhv gen t (Bhv ms) = Bhv $ map (collapseMap gen t) ms
-
-allInstances :: [Type] -> [[Type]]
-allInstances ts = go ts M.empty
-  where
-    go [] _ = [[]]
-    go (TGeneric a:ts) tbl
-      | a `M.member` tbl = map (tbl M.! a:) $ go ts tbl
-      | otherwise = concatMap (\t -> map (t:) $ go ts $ M.insert a t tbl) allTypes
-    go (t:ts) tbl = map (t:) $ go ts tbl
-
-isMatching :: [Type] -> [Type] -> M.Map String Type -> Maybe (M.Map String Type)
-isMatching [] _ m = pure m
-isMatching exp [] _ = Nothing
-isMatching (TGeneric gen:es) (g:gs) m =
-  if gen `M.member` m
-  then
-    case g of
-      TGeneric s ->
-        if s `M.member` m
-        then isMatching es gs $ M.insert gen (m M.! s) m
-        else isMatching es gs $ M.insert gen g m
-      _ -> 
-        if g == m M.! gen
-        then isMatching es gs m
-        else Nothing
-  else
-    isMatching es gs $ M.insert gen g m
-isMatching (e:es) (g:gs) m =
-  case g of
-    TGeneric s ->
-      if s `M.member` m
-      then
-        if e == m M.! s
-        then isMatching es gs m
-        else Nothing
-      else isMatching es gs $ M.insert s e m
-    _ ->
-      if e == g
-      then isMatching es gs m
-      else Nothing
-
-collapse :: [Type] -> M.Map String Type -> [Type]
-collapse ts gens = map (\case
-                           TGeneric s ->
-                             if s `M.member` gens
-                             then gens M.! s
-                             else TGeneric s
-                           t -> t) ts
-
-tryResolve :: [Mapping] -> Mapping -> [Mapping]
-tryResolve maps (inp, out) = concatMap (\(inp', out') ->
-                                          case isMatching inp' out M.empty of
-                                            Nothing -> []
-                                            Just gens -> [(collapse inp gens,
-                                                           collapse (out' ++ drop (length inp') out) gens)]) maps
-
-resolveBody :: M.Map String Behaviour -> [Instruction] -> [Type] -> Maybe Behaviour
-resolveBody known is initial = go is [(initial, initial)]
-  where
-    allInsts = allInstances initial
-    go :: [Instruction] -> [Mapping] -> Maybe Behaviour
-    go (i:is) states = trace ("On: " ++ show i) $ 
-      case i of
-        PushInt _ -> go is $ map (\(inp, out) -> (inp, TInt:out)) states
-        PushFloat _ -> go is $ map (\(inp, out) -> (inp, TFloat:out)) states
-        Call f ->
-          if f `M.member` known
-          then
-            let (Bhv maps) = known M.! f
-            in {-trace ("Maps: " ++ show maps) $-} 
-              let states' = concatMap (tryResolve maps) states
-              in go is states'
-          else Nothing
-        Exit -> pure $ Bhv states
-    go _ _ = error "Function ended without 'Exit'"
-
-generics :: Int -> [Type]
-generics n = [ TGeneric $ "G" ++ show i | i <- [1..n] ]
 
 analyzeProgram :: FuncTable -> IO ()
-analyzeProgram tbl = do
-  resolved <- foldM (\known (name, fn) ->
-                          case fn of
-                            Defined (Limited n) body ->
-                              case resolveBody known (V.toList body) (generics n) of
-                                Just bhv -> pure $ M.insert name bhv known
-                                Nothing -> do
-                                  putStrLn ("Unresolved: '" ++ name ++ "'")
-                                  return known
-                            Defined (Ellipses n) _ -> error "TODO"
-                            _ -> pure known) internals $ M.toList tbl
-  forM_ (M.toList resolved) $ \(k, Bhv maps) -> do
-    putStrLn $ "fn " ++ k ++ " instances:"
-    forM_ maps $ \(inp, out) -> do
-      putStrLn $ "  " ++ show inp ++ " -> " ++ show out
+analyzeProgram = error "TODO"
